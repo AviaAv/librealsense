f7665e0668f9c4fd7c4d3d6a397fca3328dbd138
Avia Avraham <145359432+AviaAv@users.noreply.github.com>
2025-11-27 16:37:59 +0200

    sqlite3 moved to a separate file

--- third-party/realsense-file/config.cmake
+++ third-party/realsense-file/config.cmake
@@ -23,22 +23,6 @@ set(YAML_CPP_DISABLE_UNINSTALL ON CACHE BOOL "" FORCE)
 
 FetchContent_MakeAvailable(yaml-cpp)
 
-# Force SQLite to export all symbols on Windows so the .lib is generated
-# There's probably a better way to do it but it works for now - either have the folder on the repo, or pass the flag
-# if there's no better way, we can and should restore the old flag
-set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
-
-FetchContent_Declare(
-    sqlite3
-    GIT_REPOSITORY https://github.com/sjinks/sqlite3-cmake
-    GIT_TAG v3.49.1
-)
-FetchContent_MakeAvailable(sqlite3)
-
-# Reset it so it doesn't affect other libraries
-set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS OFF) # TODO restore previous value instead of forcing OFF
-
-
 list(APPEND ROSBAG_HEADER_DIRS ${ROSBAG2_HEADER_DIRS})
 list(APPEND SOURCE_FILES_ROSBAG ${SOURCE_FILES_ROSBAG2})
 list(APPEND HEADER_FILES_ROSBAG ${HEADER_FILES_ROSBAG2})
